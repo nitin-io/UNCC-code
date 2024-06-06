86b751612a873c6ebc87b8a8e46c0d2baf1225dc
Nitin Chaudhary <nitin.io@outlook.com>
2024-06-06 09:21:04 +0530

    continuing fs: three ways for same thing

--- FileSystems/index.js
+++ FileSystems/index.js
@@ -1,10 +1,27 @@
 /********* Promise API *********/
-const fsPromise = require("node:fs/promises");
+const fsPromise = require('node:fs/promises');
 
 (async () => {
-  console.log(await fsPromise.readFile("test.txt"));
-})()
-
+  console.log(
+    (
+      'Using Promise: ' + (await fsPromise.readFile(__dirname + '/test.txt'))
+    ).toString()
+  );
+})();
 
 /********* Callback API *********/
+const fsCallback = require('node:fs');
+
+fsCallback.readFile(__dirname + '/test.txt', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log('Using Callback: ' + data);
+});
+
 /********* Sychrounous API *********/
+const fsSync = require('node:fs');
+
+const data = fsSync.readFileSync(__dirname + '/test.txt');
+console.log('Using Sychronous Call: ', data);
